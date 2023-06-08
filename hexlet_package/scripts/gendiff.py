45caabfef34f45427ed368fcfa5221434f608662
ankosde <ankos.de@gmil.com>
2023-06-08 10:23:11 +0300

    all

--- hexlet_package/scripts/gendiff.py
+++ hexlet_package/scripts/gendiff.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
-
-from hexlet_package import user
+import argparse
 
 
 def main():
-    print(user.print_certificate()) 
+    print("Hello!")
+
+parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
+parser.add_argument('first_file')
+parser.add_argument('second_file')
+args = parser.parse_args()
 
 
 if __name__ == '__main__':
